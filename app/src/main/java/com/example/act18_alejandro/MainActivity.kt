e160350e6cdafecaccbef30a980fc4431b1443b3
AlejandroBaldeonEspinoza <a24josbalesp@inspedralbes.cat>
2026-03-19 19:43:18 +0100

    Version de Gradle arreglada. Boton para Product Manager funcional

--- app/src/main/java/com/example/act18_alejandro/MainActivity.kt
+++ app/src/main/java/com/example/act18_alejandro/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.act18_alejandro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val menuShop = findViewById<Button>(R.id.buttonShop)
+        menuShop.setOnClickListener{
+            val intent = Intent(this, ProductsManager::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
