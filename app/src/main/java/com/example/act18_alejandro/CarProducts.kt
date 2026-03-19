2fd021f518ce8cafb97d913d2c4d9431f3990dc0
AlejandroBaldeon <a24josbalesp@inspedralbes.cat>
2026-03-19 20:08:49 +0100

    Data class Resumen carrito de compras hecho

---
+++ app/src/main/java/com/example/act18_alejandro/CarProducts.kt
@@ -0,0 +1,9 @@
+package com.example.act18_alejandro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+data class CarProducts(
+    val product: Product,
+    val cantidad: Int=0
+)
